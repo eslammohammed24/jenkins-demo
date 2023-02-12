73c6b890706ed72b305a131ddc7a272381565d29
eslammohammed24 <112381364+eslammohammed24@users.noreply.github.com>
2023-02-12 22:09:13 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,16 @@
 def buildApp() {
-    echo 'building the application...'
+    echo 'building the application'
+    echo "${params.namespace}"
 } 
 
 def testApp() {
-    echo 'testing the application...'
+    echo 'testing the application'
 } 
 
 def deployApp() {
     echo 'deplying the application...'
     echo "deploying version ${params.VERSION}"
+    echo "this is build number $BUILD_NUMBER"
 } 
 
 return this
